09105aba9cef26daa7947b27455e247459a11838
Alexander Vitchinov <vitalex92@yandex.ru>
2020-09-05 14:53:32 +0300

    second release

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const popupOpen = document.querySelector('.js-open__info-edit');
 const popupClose = document.querySelector('.js-popup__close');
 const formElement = document.querySelector('.popup__form');
 const popupSave = document.querySelector('.js-popup__submit');
-let nameInput = formElement.querySelector('.js-popup__form-name');
-let jobInput = formElement.querySelector('.js-popup__form-job');
+let nameInput = formElement.querySelector('.js-popup__name');
+let jobInput = formElement.querySelector('.js-popup__job');
 let profileName = document.querySelector('.profile__info-name');
 let profileJob = document.querySelector('.profile__info-job');
 

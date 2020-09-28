42e27fd8425f2fe234e03da35930edc04d663f90
Alexander Vitchinov <vitalex92@yandex.ru>
2020-09-28 22:17:22 +0300

    start

---
+++ validate.js
@@ -0,0 +1,20 @@
+const enableValidation = ({ formSelector }) => {
+  console.log(formSelector);
+}
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+});
+
+export default enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit',
+  inactiveButtonClass: 'popup__submit_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+})

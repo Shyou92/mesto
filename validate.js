294f90f9218ccea5770593f43b9eec878fc11d3b
Alexander Vitchinov <vitalex92@yandex.ru>
2020-09-30 00:03:27 +0300

    difficult start

--- validate.js
+++ validate.js
@@ -1,36 +1,52 @@
-const setEventlisteners = (formElement) => {
-  const inputList = Array.from(inputSelector);
-
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', () => {
-      console.log('1');
-    })
-  })
-}
-
-const enableValidation = ({ formSelector }) => {
-  const formList = Array.from(formSelector);
-
-  formList.forEach((formElement) => {
-    formElement.addEventListener('submit', (e) => {
-      e.preventDefault();
-    });
-  })
-}
-enableValidation({
+const allSelectorsClasses = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__button',
   inactiveButtonClass: 'popup__button_disabled',
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible'
-});
+}
+console.log(allSelectorsClasses.formSelector);
 
-export default enableValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__submit',
-  inactiveButtonClass: 'popup__submit_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
-})
+const showInputError = (formElement, inputItem, allClasses, errorMessage) => {
+  const errorElement = formElement.querySelector(`#${inputItem.id}-error`);
+  console.log(errorElement);  // добавь в css показать и скрыть ошибку и допиши ф-ции
+};
+
+const hideInputErrror = (formElement, inputItem, allClasses) => { };
+
+const checkInputValidity = (formElement, inputItem, allClasses) => {
+  console.log(inputItem.validity);
+  const isInputNotValid = inputItem.validity.valid;
+
+  if (isInputNotValid) {
+    const errorMessage = inputItem.validationMessage;
+
+    showInputError(formElement, inputItem, allClasses, errorMessage)
+  } else {
+    hideInputErrror(formElement, inputItem, allClasses)
+  }
+}
+
+const setEventListeners = (formElement, allClasses) => {
+  const inputList = Array.from(formElement.querySelectorAll(allClasses.inputSelector));
+  inputList.forEach((inputItem) => {
+    inputItem.addEventListener('input', (e) => {
+      console.log(e.target.name);
+      checkInputValidity(formElement, inputItem, allSelectorsClasses)
+    })
+  })
+}
+
+
+const enableValidation = (allClasses) => {
+  const formList = Array.from(document.querySelectorAll(allClasses.formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (e) => {
+      e.preventDefault()
+    })
+    setEventListeners(formElement, allSelectorsClasses)
+  })
+}
+
+enableValidation(allSelectorsClasses);
